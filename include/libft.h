d3cec037c633f8b9c47eeb0e9b548c40eae0ae1c
Armand Larzilliere <arlarzil@paul-f4Br3s7.clusters.42paris.fr>
2023-11-06 11:02:55 +0100

    Added ft_strlen ft_putchar_fd ft_putstr_fd ft_putnbr_fd ft_putendl_fd

---
+++ include/libft.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arlarzil <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/06 10:36:34 by arlarzil          #+#    #+#             */
+/*   Updated: 2023/11/06 11:02:16 by arlarzil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_putendl_fd(char *s, int fd);
+
+size_t	ft_strlen(char *s);
+
+#endif
