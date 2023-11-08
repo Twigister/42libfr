7a6767bf8a5bdf2c88caae9149846730b055c674
Armand Larzilliere <arlarzil@bess-f2r4s6.clusters.42paris.fr>
2023-11-08 18:47:40 +0100

    Added bonus functions

---
+++ src/bonus/ft_lstadd_back.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arlarzil <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/08 16:23:12 by arlarzil          #+#    #+#             */
+/*   Updated: 2023/11/08 16:23:14 by arlarzil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	if (!*lst)
+		*lst = new;
+	else if (!lst->next)
+		lst->next = new;
+	else
+		ft_lstadd_back(lst->next, new);
+}
---
+++ src/bonus/ft_lstmap.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arlarzil <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/08 16:24:44 by arlarzil          #+#    #+#             */
+/*   Updated: 2023/11/08 16:24:45 by arlarzil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*res;
+	t_list	*new_node;
+
+	res = NULL;
+	while (lst)
+	{
+		new_node = ft_lstnew((*f)(lst->data));
+		if (!new_node)
+		{
+			ft_lstclear(&res, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&res, new_node);
+		lst = lst->next;
+	}
+	return (res);
+}
